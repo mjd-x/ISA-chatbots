4952ecbe273fdf98acc18d23d7a2892092df040a
mjd-x <marina_jeanette@hotmail.com>
2021-12-14 22:29:43 -0300

    MOD base de datos

--- mjd_chatbots/settings.py
+++ mjd_chatbots/settings.py
@@ -82,13 +82,23 @@ AUTH_USER_MODEL = 'chat.User'  # para que tome este modelo para la autenticacion
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'chatbots',
+        'USER': 'postgres',
+        'PASSWORD': 'informatica2021',
+        'HOST': '35.223.54.93',
+        'PORT': '5432'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
